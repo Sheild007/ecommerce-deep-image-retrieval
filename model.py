200cc744c742de6b3ff5ee9bcf70691177d9c26f
Usman <bscs22008@itu.edu.pk>
2026-03-20 06:47:40 +0500

    feat:model done

---
+++ model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn 
+import torch.nn.functional as F 
+import torchvision.models as models 
+
+
+class EmbeddingNet(nn.Module):
+    def __init__(self, embeding_dim:int=128) -> None:
+        super().__init__()
+        #Resnet50 with pretrained weights as backbone feature_extractor
+        backbone=models.Resnet50(weights=models.Resnet50_weights.DEFAULT)
+        input_dim=backbone.fc.in_features 
+        #striping final fc layer from backbone 
+        backbone.fc=nn.Identity()
+        self.feature_extractor=backbone
+        #Fully Connected Layer(2048 -> 128)
+        self.fc=nn.Linear(input_dim,embeding_dim)
+
+    def forward(self,x):
+        # step1: extaract features using Resnet50
+        features=self.feature_extractor(x)
+        #step2: prohect fetaures to 128 embedding vector 
+        embeddings=self.fc(features)
+        #step3: L2 normalization
+        embeddings=F.normalize(embeddings) # by default L2 on rows 
+        return embeddings
+        
+
